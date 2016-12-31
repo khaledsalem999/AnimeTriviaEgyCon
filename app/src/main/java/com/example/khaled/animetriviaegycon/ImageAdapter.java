e06ad1a82c14b7b15754bdf2efdf56561b7f3e1d
khaledsalem999 <khaledsalem999@gmail.com>
2016-12-31 02:02:34 +0200

    New Image Adaptro class Changed some colors

--- app/src/main/java/com/example/khaled/animetriviaegycon/ImageAdapter.java
+++ app/src/main/java/com/example/khaled/animetriviaegycon/ImageAdapter.java
@@ -1,58 +1,83 @@
 package com.example.khaled.animetriviaegycon;
 
 import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Khaled on 12/8/2016.
  */
 public class ImageAdapter extends BaseAdapter {
-    private Context mContext;
-    private Integer[] mThumbIds = new Integer[50];
+    private final List<Item> mItems = new ArrayList<Item>();
+    private final LayoutInflater mInflater;
 
-    public ImageAdapter(Context c) {
-        mContext = c;
+    public ImageAdapter(Context context) {
+        mInflater = LayoutInflater.from(context);
+
+        for(int x = 0;x<5;x++) {
+            mItems.add(new Item("Naruto", R.mipmap.naruto));
+            mItems.add(new Item("Bleach", R.mipmap.bleach));
+        }
     }
 
+    @Override
     public int getCount() {
-        return mThumbIds.length;
+        return mItems.size();
     }
 
-    public Object getItem(int position) {
-        return null;
+    @Override
+    public Item getItem(int i) {
+        return mItems.get(i);
     }
 
-    public long getItemId(int position) {
-        return 0;
+    @Override
+    public long getItemId(int i) {
+        return mItems.get(i).drawableId;
     }
 
-    // create a new ImageView for each item referenced by the Adapter
-    public View getView(int position, View convertView, ViewGroup parent) {
-        ImageView imageView;
-        if (convertView == null) {
-            // if it's not recycled, initialize some attributes
-            imageView = new ImageView(mContext);
-            imageView.setLayoutParams(new GridView.LayoutParams(200, 300));
-            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-        } else {
-            imageView = (ImageView) convertView;
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        Log.e("Your current location:", Integer.toString(i));
+        View v = view;
+        ImageView picture;
+        TextView name;
+
+        if (v == null) {
+            v = mInflater.inflate(R.layout.grid_item, viewGroup, false);
+            v.setTag(R.id.picture, v.findViewById(R.id.picture));
+            v.setTag(R.id.text, v.findViewById(R.id.text));
         }
 
+        picture = (ImageView) v.getTag(R.id.picture);
+        name = (TextView) v.getTag(R.id.text);
 
-        for(int i=0;i<50;i++){
-            mThumbIds[i] = R.mipmap.naruto;
-        }
+        Item item = getItem(i);
 
-        imageView.setImageResource(mThumbIds[position]);
-        return imageView;
+        picture.setImageResource(item.drawableId);
+        name.setText(item.name);
+
+        return v;
     }
 
-    // references to our images
+    private static class Item {
+        public final String name;
+        public final int drawableId;
+
+        Item(String name, int drawableId) {
+            this.name = name;
+            this.drawableId = drawableId;
+        }
+    }
 
 
 
