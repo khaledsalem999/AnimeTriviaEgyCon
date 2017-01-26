f68c97d7faf549bcdddb239888e0210d3e21ca0b
hyhae <hussein132942@miuegypt.edu.eg>
2017-01-26 19:12:02 +0200

    local files - done

--- app/src/main/java/com/example/khaled/animetriviaegycon/PicassoClient.java
+++ app/src/main/java/com/example/khaled/animetriviaegycon/PicassoClient.java
@@ -1,9 +1,12 @@
 package com.example.khaled.animetriviaegycon;
 
 import android.content.Context;
+import android.util.Log;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
+
+import static android.R.attr.id;
 
 /**
  * Created by hyhae on 12/31/2016.
@@ -11,11 +14,20 @@ import com.squareup.picasso.Picasso;
 
 public class PicassoClient {
     public static void downloadImage(Context c, String url, ImageView img){
-        if(url != null && url.length()>0){
-            Picasso.with(c).load(url).placeholder(R.mipmap.placeholder).into(img);
+        if(url != null && url.length()>0) {
+
+            int id = 0;
+            if (url.contains("http") != true) {
+                String name = url;
+                id = c.getResources().getIdentifier(name, "mipmap", c.getPackageName());
+                Picasso.with(c).load(id).placeholder(R.drawable.placeholder).into(img);
+            }
+            else{
+                Picasso.with(c).load(url).placeholder(R.drawable.placeholder).into(img);
+            }
         }
         else{
-            Picasso.with(c).load(R.mipmap.placeholder).into(img);
+            Picasso.with(c).load(R.drawable.placeholder).into(img);
         }
     }
 }
